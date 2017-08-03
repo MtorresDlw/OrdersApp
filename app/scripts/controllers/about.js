575d0e559c733248d075cfb83fd40a49817a401f
mtorres <migueltorres@orange.fr>
2017-08-03 09:19:42 +0200

    Initialisation du projet OrdersApp

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ordersAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ordersAppApp
+ */
+angular.module('ordersApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
